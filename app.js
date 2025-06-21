026078e5246ad5b80c58d2a36c14d2b028e58b9e
NoumanSharif27 <noumansharifgul27@gmail.com>
2025-06-21 10:44:00 +0500

    completed the Error handeling

--- app.js
+++ app.js
@@ -31,12 +31,18 @@ let Authentication = (req, res, next) => {
 };
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.set("view engine", "views");
 app.set("views", path.join(__dirname, "views"));
 
 // app.engine("ejs", ejsMate);
+
+function aysncWrap(fn) {
+  return function (req, res, next) {
+    fn();
+  };
+}
 
 app.get("/", (req, res) => {
   console.log(req.url);
@@ -48,10 +54,25 @@ app.get("/random", Authentication, (req, res) => {
   res.send(`Welcome to random`);
 });
 
-app.get("/chat", async (req, res) => {
-  const chats = await Chat.find();
-  res.render("index.ejs", chats);
-});
+app.get(
+  "/chats",
+  aysncWrap(async (req, res) => {
+    const chats = await Chat.find();
+    res.render("index.ejs", { chats });
+  })
+);
+
+app.get(
+  "/chats/:id",
+  asyncWrap(async (req, res, next) => {
+    const { id } = req.params;
+    const chat = await Chat.findById(id);
+    if (!chat) {
+      return next(new ExpressError(404, "Chat not found!"));
+    }
+    res.render("show.ejs", { chat });
+  })
+);
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Some error occured!" } = err;
